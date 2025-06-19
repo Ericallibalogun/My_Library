d3b4e47a9e13c16d41904953d95567348763b2dc
Ericalli <allieric28@gmail.com>
2025-06-19 09:30:46 +0100

    commit

--- MyLibrary/src/main/java/org/africa/semicolon/mylibrary/data/models/Book.java
+++ MyLibrary/src/main/java/org/africa/semicolon/mylibrary/data/models/Book.java
@@ -1,5 +1,6 @@
 package org.africa.semicolon.mylibrary.data.models;
 
+
 import lombok.Data;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
